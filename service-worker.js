83b016deffd71c6c2a0e7ce2ceb1f867f136b1a2
Htx1980 <adrianarendela113@gmail.com>
2025-03-21 20:17:41 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,15 +9,33 @@ const OFFLINE_FILES = [
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
+            return cache.addAll(OFFLINE_FILES);
         })
     );
+    self.skipWaiting(); 
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim(); 
 });
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).catch(() => {
+                return caches.match("index.html"); 
+            });
         })
     );
 });
